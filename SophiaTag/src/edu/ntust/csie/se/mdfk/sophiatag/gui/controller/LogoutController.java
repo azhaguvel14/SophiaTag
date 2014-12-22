0fab4cb1312d0d8c937e3cc6b3b9e67f0da965ba
CY <zxp950977@gmail.com>
2014-12-22 10:48:44 +0800

    #2014/12/22 我完成logout了 ^-^

---
+++ SophiaTag/src/edu/ntust/csie/se/mdfk/sophiatag/gui/controller/LogoutController.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package edu.ntust.csie.se.mdfk.sophiatag.gui.controller;
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.JOptionPane;
+
+import edu.ntust.csie.se.mdfk.sophiatag.gui.controller.bundle.LoginBundle;
+import edu.ntust.csie.se.mdfk.sophiatag.gui.view.LoginView;
+import edu.ntust.csie.se.mdfk.sophiatag.gui.view.MainView;
+import edu.ntust.csie.se.mdfk.sophiatag.service.SophiaTagServices;
+
+/**
+ * @author cy
+ *
+ */
+public class LogoutController implements MainViewActionEventController {
+
+	@Override
+	public void handle(ActionEvent event, SophiaTagServices services, MainView view) {
+		int userDecision =
+		JOptionPane.showConfirmDialog(view.getFrame(), "Are you sure to logout?", "Logout Confirm", JOptionPane.YES_NO_OPTION);
+		
+		switch(userDecision)
+		{
+			case JOptionPane.YES_OPTION:
+				services.saveRecord();
+				view.getFrame().dispose();
+				LoginView loginForm = new LoginView(services.isServiceReady());
+				MVCGlue.glue(services, loginForm, new LoginBundle());
+				loginForm.getFrame().setVisible(true);
+				break;
+			case JOptionPane.NO_OPTION:				
+		}
+	}
+
+}
--- SophiaTag/src/edu/ntust/csie/se/mdfk/sophiatag/gui/controller/bundle/MainBundle.java
+++ SophiaTag/src/edu/ntust/csie/se/mdfk/sophiatag/gui/controller/bundle/MainBundle.java
@@ -7,6 +7,7 @@ import java.awt.event.WindowEvent;
 
 import javax.swing.event.ListSelectionEvent;
 
+import edu.ntust.csie.se.mdfk.sophiatag.gui.controller.LogoutController;
 import edu.ntust.csie.se.mdfk.sophiatag.gui.controller.MaterialSelectedController;
 import edu.ntust.csie.se.mdfk.sophiatag.gui.controller.WindowClosingController;
 import edu.ntust.csie.se.mdfk.sophiatag.gui.tagbutton.TagButton.TextChangedEvent;
@@ -23,7 +24,7 @@ public class MainBundle extends ControllerBundle<MainView> {
 		this.addController("window_closing", WindowEvent.class, new WindowClosingController());
 		
 		//replace null with the controller instance that you implement, you can refer to LoginBundle class 
-		this.addActionEventController("logout", null); //Tung
+		this.addActionEventController("logout", new LogoutController()); //Tung
 		this.addActionEventController("change_root_dir", null); //紀
 		this.addActionEventController("search", null); //紀
 		this.addActionEventController("open_dir", null); //常
