6b4dc35313471ae58a814b69941998e29637228f
maeglin89273 <maeglin89273@gmail.com>
2014-12-23 22:27:31 +0800

    #Solve conflict of MainBundle

--- SophiaTag/src/edu/ntust/csie/se/mdfk/sophiatag/gui/controller/bundle/MainBundle.java
+++ SophiaTag/src/edu/ntust/csie/se/mdfk/sophiatag/gui/controller/bundle/MainBundle.java
@@ -7,11 +7,16 @@ import java.awt.event.WindowEvent;
 
 import javax.swing.event.ListSelectionEvent;
 
+import edu.ntust.csie.se.mdfk.sophiatag.data.RemoveTagController;
+import edu.ntust.csie.se.mdfk.sophiatag.gui.controller.AddTagController;
+import edu.ntust.csie.se.mdfk.sophiatag.gui.controller.EditTagController;
 import edu.ntust.csie.se.mdfk.sophiatag.gui.controller.LogoutController;
 import edu.ntust.csie.se.mdfk.sophiatag.gui.controller.MainViewInitController;
 import edu.ntust.csie.se.mdfk.sophiatag.gui.controller.MaterialSelectedController;
+import edu.ntust.csie.se.mdfk.sophiatag.gui.controller.OpenDirectoryController;
 import edu.ntust.csie.se.mdfk.sophiatag.gui.controller.SearchController;
 import edu.ntust.csie.se.mdfk.sophiatag.gui.controller.WindowClosingController;
+import edu.ntust.csie.se.mdfk.sophiatag.gui.custom.tagbutton.TagButton.RemoveTagEvent;
 import edu.ntust.csie.se.mdfk.sophiatag.gui.custom.tagbutton.TagButton.TextChangedEvent;
 import edu.ntust.csie.se.mdfk.sophiatag.gui.view.MainView;
 
@@ -30,13 +35,14 @@ public class MainBundle extends ControllerBundle<MainView> {
 		this.addActionEventController("logout", new LogoutController());
 		this.addActionEventController("change_root_dir", null); //紀
 		this.addActionEventController("search", new SearchController()); //紀
-		this.addActionEventController("open_dir", null); //CoZy
-		this.addActionEventController("add_tag", null); //常
-		this.addActionEventController("remove_tag", null);//常
+		this.addActionEventController("open_dir", new OpenDirectoryController()); //CoZy
+		this.addActionEventController("add_tag", new AddTagController()); //常
+		//常
 		this.addActionEventController("discard_material", null); //CoZy
 		
 		this.addController("material_selected", ListSelectionEvent.class, new MaterialSelectedController());
-		this.addController("edit_tag", TextChangedEvent.class, null);//常
+		this.addController("edit_tag", TextChangedEvent.class, new EditTagController());//常
+		this.addController("remove_tag", RemoveTagEvent.class, new RemoveTagController());
 	}
 
 }
